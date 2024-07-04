32467d5eb5db1153fc194fbc2f0ed23692b79745
rakhidesale <rakhidesale2002@gmail.com>
2024-07-04 14:35:44 +0530

    Bug fixes

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props) => {
-
+const News = ({ title = 'Default Title', content = 'Default Content', country, category, apiKey, pageSize, setProgress }) => {
+  
   const [articles, setArticles]= useState([])
   const [loading, setLoading]= useState(true)
   const [page, setPage]= useState(1)
@@ -16,27 +16,27 @@ const News = (props) => {
   };
 
   const updateNews = async () => {
-    props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
     setLoading(true);
     let data = await fetch(url);
-    props.setProgress(30);
-    let parseData = await data.json();
-    props.setProgress(70);
-    setArticles(parseData.articles)
-    setTotalResults(parseData.totalResults)
+    setProgress(30);
+    let parsedData = await data.json();
+    setProgress(70);
+    setArticles(parsedData.articles)
+    setTotalResults(parsedData.totalResults)
     setLoading(false)
-    props.setProgress(100);
+    setProgress(100);
   }
 
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - NewsFlash`;
+    document.title = `${capitalizeFirstLetter(category)} - NewsFlash`;
     updateNews();
     // eslint-disable-next-line
   }, [])
 
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page+1}&pageSize=${pageSize}`;
     setPage(page+1)
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -47,7 +47,7 @@ const News = (props) => {
     return (
       <>
         <h2 className="text-center" style={{ margin: "35px 0px",  marginTop:'90px', fontFamily:'Georgia, serif', color:'#4C3C3C'}}>
-          NewsFlash - Top {capitalizeFirstLetter(props.category)}{" "}
+          NewsFlash - Top {capitalizeFirstLetter(category)}{" "}
           Headlines
         </h2>
         {loading && <Spinner/>}
